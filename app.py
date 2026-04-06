bc7e52092dcbc5517f5d9c174793bf9c83773e5d
weijun88 <leeweijun76@yahoo.com>
2026-04-06 07:27:57 +0000

    1

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def reply():
         "content": q
     }]
 )
-    return(render_template('reply.hyml',r=rr.choices[0].message.content))
+return(render_template('reply.hyml',r=rr.choices[0].message.content))
 
 if __name__ == "__main__":
     app.run()
